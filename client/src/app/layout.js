bb6bb1e014e9fe1e09e10845d6cd6cfa9b5b4bef
AbhishekGour12 <aggour124421@gmail.com>
2026-02-26 13:41:24 +0530

    first commit

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -47,6 +47,33 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-RMNVENG1P5');
           `}
         </Script>
+        {/* Facebook Pixel */}
+<Script
+  id="facebook-pixel"
+  strategy="afterInteractive"
+>
+  {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '2770032406663985');
+    fbq('track', 'PageView');
+  `}
+</Script>
+
+<noscript>
+  <img
+    height="1"
+    width="1"
+    style={{ display: "none" }}
+    src="https://www.facebook.com/tr?id=2770032406663985&ev=PageView&noscript=1"
+  />
+</noscript>
         <PathLayout> 
         <Providers className="pt-[120px]">
           <CartProvider>
