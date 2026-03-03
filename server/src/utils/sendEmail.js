19e7f902686443772a302ebff22377573075bfe1
AbhishekGour12 <aggour124421@gmail.com>
2026-03-03 16:38:03 +0530

    first  commit

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER || "aggour124421@gmail.com",
-    pass: process.env.EMAIL_PASS || "vupf wimf scix xtse",
+    user: process.env.EMAIL_USER || "myastrova@gmail.com",
+    pass: process.env.EMAIL_PASS || "ksiv xrfv aghc ecit"
   },
 });
 
